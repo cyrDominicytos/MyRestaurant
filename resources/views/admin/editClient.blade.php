03c54f95761dba74eee3e2e7d2dea881415b8e3c
PatrickOssounlanan <patrickolouchola1@gmail.com>
2022-03-21 18:18:02 +0100

    update

--- resources/views/admin/editClient.blade.php
+++ resources/views/admin/editClient.blade.php
@@ -14,7 +14,7 @@
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Enregistrer Client</li>
+              <li class="breadcrumb-item active">Modifier  Utilisateur</li>
             </ol>
           </div>
         </div>
